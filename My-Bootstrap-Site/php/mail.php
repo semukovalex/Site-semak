e2e8d3ede50f3188604dccfaa6a72cf520481b8f
Пользователь <sashasemukov@gmail.com>
2020-06-05 14:59:27 +0300

    Изменения(05.06.20)

--- My-Bootstrap-Site/php/mail.php
+++ My-Bootstrap-Site/php/mail.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title>Обратная связь</title>
-</head>
-<body>
 <?php
-if (isset($_POST['name'])) {$name = $_POST['name'];}
-if (isset($_POST['email'])) {$email = $_POST['email'];}
-if (isset($_POST['mess'])) {$mess = $_POST['mess'];}
-
-$to = "bobeshmo@mail.ru"; /*Укажите ваш адрес электоронной почты*/
-$headers = "Content-type: text/plain; charset=utf-8";
-$subject = "Сообщение с вашего сайта";
-$message = "Имя пославшего: $name \nЭлектронный адрес: $email \nСообщение: $mess";
-$send = mail ($to, $subject, $message, $headers);
-if ($send == 'true')
-{
-echo "<b>Спасибо за отправку вашего сообщения!<p>";
-echo "<a href=index.html>Нажмите,</a> чтобы вернуться на главную страницу";
-}
-else
-{
-echo "<p><b>Ошибка. Сообщение не отправлено!";
-}
+$to = "bobeshmo@mail.ru"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Тема: ".$_POST['header']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['mess']."<br>"; //полученное из формы name=message
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
 ?>
-</body>
-</html>
